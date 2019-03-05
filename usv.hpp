55356641c657f9f74d6036dbda9942ba8f7399cd
R. Martinho Fernandes <cpp@rmf.io>
2019-03-05 08:43:25 +0100

    Use factory instead of throwing constructor

--- usv.hpp
+++ usv.hpp
@@ -1,25 +1,14 @@
 #pragma once
 
 #include <cstdint>
+#include <optional>
 
 namespace sg16 {
 
-constexpr auto first_surrogate = 0xD800;
-constexpr auto last_surrogate = 0xDFFF;
-constexpr auto last_code_point = 0x10FFFF;
-
-struct {} constexpr bad_scalar_value = {};
 struct {} constexpr assume_valid = {};
 
 class scalar_value {
 public:
-    explicit constexpr scalar_value(std::uint32_t value)
-    : value(value) {
-        if ((value >= first_surrogate && value <= last_surrogate) || value > last_code_point) {
-            throw bad_scalar_value;
-        }
-    }
-
     explicit constexpr scalar_value(std::uint32_t value, decltype(assume_valid)) noexcept
     : value(value) {}
 
@@ -36,5 +25,17 @@ public:
 private:
     std::uint32_t value;
 };
+
+constexpr auto first_surrogate = 0xD800;
+constexpr auto last_surrogate = 0xDFFF;
+constexpr auto last_code_point = 0x10FFFF;
+
+constexpr std::optional<scalar_value> as_scalar_value(std::uint32_t value) noexcept {
+    if ((value >= first_surrogate && value <= last_surrogate) || value > last_code_point) {
+        return std::nullopt;
+    } else {
+        return scalar_value(value, assume_valid);
+    }
+}
 
 }
