4b601e59f4f34d453346d91d7e4aca9ff4473a95
R. Martinho Fernandes <cpp@rmf.io>
2019-03-05 08:57:15 +0100

    Use factories for both constructions

--- usv.hpp
+++ usv.hpp
@@ -7,10 +7,23 @@ namespace sg16 {
 
 struct {} constexpr assume_valid = {};
 
+constexpr std::optional<scalar_value> as_scalar_value(std::uint32_t value) noexcept;
+constexpr scalar_value as_scalar_value(std::uint32_t value, decltype(assume_valid)) noexcept;
+
 class scalar_value {
 public:
-    explicit constexpr scalar_value(std::uint32_t value, decltype(assume_valid)) noexcept
-    : value(value) {}
+    friend constexpr std::optional<scalar_value> as_scalar_value(std::uint32_t value) noexcept {
+        if ((value >= first_surrogate && value <= last_surrogate) || value > last_code_point) {
+            return std::nullopt;
+        } else {
+            [[likely]]
+            return scalar_value { value };
+        }
+    }
+
+    friend constexpr scalar_value as_scalar_value(std::uint32_t value, decltype(assume_valid)) noexcept {
+        return scalar_value { value };
+    }
 
     explicit constexpr operator std::uint32_t() const noexcept {
         return value;
@@ -29,14 +42,5 @@ private:
 constexpr auto first_surrogate = 0xD800;
 constexpr auto last_surrogate = 0xDFFF;
 constexpr auto last_code_point = 0x10FFFF;
-
-constexpr std::optional<scalar_value> as_scalar_value(std::uint32_t value) noexcept {
-    if ((value >= first_surrogate && value <= last_surrogate) || value > last_code_point) {
-        return std::nullopt;
-    } else {
-        [[likely]]
-        return scalar_value(value, assume_valid);
-    }
-}
 
 }
